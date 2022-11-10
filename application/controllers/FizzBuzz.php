4819528ca927f76cbf71dcaa72edd4bc7fc2dd87
Are1PM <arwanpriantomangidi@gmail.com>
2022-11-10 10:14:06 +0800

    FizzBuzz

--- application/controllers/FizzBuzz.php
+++ application/controllers/FizzBuzz.php
@@ -4,15 +4,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class FizzBuzz extends CI_Controller {
     public function index()
 	{
-		$node = 0;
-        $fizz = 0;
-        $buzz = 0;
-        $this->fizzbuzz($node, $fizz, $buzz);
+		$node = 100;
+        $fizz = 2;
+        $buzz = 3;
+        $this->fizzbuzz_execute($node, $fizz, $buzz);
 	}
 
-    public function fizzbuzz($x, $y, $z) 
+    public function fizzbuzz_execute($x, $y, $z) 
     {
         // TO DO CODE
+        // Nama fungsi saya ubah karena terjadi error :
+        // Methods with the same name as their class will not be constructors in a future version of PHP; FizzBuzz has a deprecated constructor
+        $result = [];
 
+        if($x == 0){
+            echo "Node = 0";
+        }else{
+            
+            for($i = 1; $i <= $x; $i++){
+                if($i % $y == 0){
+                    $result[] = 'Fizz';
+                }elseif($i % $z == 0){
+                    $result[] = 'Buzz';
+                }else{
+                    $result[] = $i;
+                }
+            }
+    
+            echo implode(', ', $result);
+        }
     }
 }
