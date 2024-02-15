b5ccf227e687d9665b7e584b4689f3134a28e446
Are1PM <arwanpriantomangidi@gmail.com>
2024-02-15 15:51:19 +0800

    perbaikan

--- application/controllers/Palindrome.php
+++ application/controllers/Palindrome.php
@@ -1,39 +1,39 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Palindrome extends CI_Controller {
-    public function index()
+class Palindrome extends CI_Controller
+{
+	public function index()
 	{
-		$wordList = ['Aku','KaMu', 'MalAm','isi', '']; 
-        $this->palindrome_execute($wordList);
+		// $wordList = ['Aku','KaMu', 'MalAm','isi', '', 'Aibohphobia', 'leveL','rotator']; 
+		$wordList = ['Kasur ini rusak'];
+		$this->palindrome_execute($wordList);
 	}
 
-    public function palindrome_execute($x) 
-    {
-        // TO DO CODE
-        $result = [];
-        foreach ($x as $kata) {
-            $length = strlen($kata);
-            $k = strtolower($kata);
+	public function palindrome_execute($x)
+	{
+		// TO DO CODE
+		$result = [];
+		foreach ($x as $kata) {
+			$length = strlen($kata);
+			$k = strtolower($kata);
 
-            if($kata != null){
-                
-                $statement = 'adalah palindrome';
-                for($i = 0; $i < $length/2; $i++){
-                    
-                    if($k[$i] !== $k[$length - 1 - $i] ){
-                        $statement = 'bukan palindrome';
-                    }
-                }
-                
-            }else{
-                $statement = 'bukan palindrome';
-            }
+			if ($kata != null) {
 
-            $result[] = $kata.' '. $statement;
+				$statement = 'adalah palindrome';
+				for ($i = 0; $i < $length / 2; $i++) {
 
-        }
+					if ($k[$i] !== $k[$length - 1 - $i]) {
+						$statement = 'bukan palindrome';
+					}
+				}
+			} else {
+				$statement = 'bukan palindrome';
+			}
 
-        echo implode(', ', $result);
-    }
+			$result[] = $kata . ' ' . $statement;
+		}
+
+		echo implode(', ', $result);
+	}
 }
